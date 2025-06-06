0efd8e10d7edf61b1c023902bd1f91ad2cab0563
Angel <aal51282@uga.edu>
2025-06-06 16:17:51 -0400

    Movie or show option that is commented out.

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -25,7 +25,9 @@ const MovieCard = ({
           resizeMode="cover"
         />
 
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
 
         <View className="flex-row items-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4" />
@@ -38,6 +40,9 @@ const MovieCard = ({
           <Text className="text-xs text-light-300 font-medium mt-1">
             {release_date?.split("-")[0]}
           </Text>
+          {/* <Text className="text-xs font-medium text-light-300 uppercase">
+            Movie
+          </Text> */}
         </View>
       </TouchableOpacity>
     </Link>
