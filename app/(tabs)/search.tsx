e8804b7935bfe39f88c63c737ca725a601460a23
Angel <aal51282@uga.edu>
2025-06-07 02:54:10 -0400

    Ignore the last commit I changed this back to false because the bug no longer exists.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -16,7 +16,7 @@ const Search = () => {
     error,
     refetch: loadMovies,
     reset,
-  } = useFetch(() => fetchMovies({ query: searchQuery }), true);
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
