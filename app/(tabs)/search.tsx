b0a6ed61e7a7136aa38cceb7184bcbfd9e9895c9
Angel <aal51282@uga.edu>
2025-06-07 02:16:20 -0400

    Added movies to search page and UI for search page.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,8 +1,21 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import React from "react";
-import { Image, View } from "react-native";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React, { useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 
-const search = () => {
+const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -10,8 +23,57 @@ const search = () => {
         className="flex-1 absolute w-full z-0"
         resizeMode="cover"
       />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className="my-5">
+              <SearchBar placeholder="Search for a movie" />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000FF"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading &&
+              !error &&
+              "SEARCH TERM".trim() &&
+              movies?.length > 0 && (
+                <Text className="text-xl text-white font-bold">
+                  Search results for{" "}
+                  <Text className="text-accent">SEARCH TERM</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
     </View>
   );
 };
 
-export default search;
+export default Search;
